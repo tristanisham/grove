678d36b16537b7c56662935849ea91bcd2442f6c
Tristan Isham <tristan.isham@hey.com>
2022-04-04 00:59:21 -0400

    updated layout

--- app/browse.go
+++ app/browse.go
@@ -54,3 +54,22 @@ func (m model) View() string {
 	return docStyle.Render(m.List.View())
 }
 
+type Plant struct {
+	title, desc, author, website string
+}
+
+func (p Plant) Title() string       { return p.title }
+func (p Plant) Description() string { return p.desc }
+func (p Plant) FilterValue() string { return p.title }
+func (p Plant) Website() string     { return p.website }
+func (p Plant) Author() string      { return p.author }
+
+
+func NewPlant(title, desc, author, website string) Plant {
+	return Plant{
+		title:   title,
+		desc:    desc,
+		author:  author,
+		website: website,
+	}
+}

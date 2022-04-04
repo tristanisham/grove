6d00e372191efa76c05d09215f62b85d755de650
Tristan Isham <tristan.isham@hey.com>
2022-04-04 00:46:12 -0400

    documentation

--- app/root.go
+++ app/root.go
@@ -7,6 +7,9 @@ type groveConfig struct {
 
 type groveServerConfig struct {
 	PackageDirs []string `json:"packageDirs"`
+	BlackListIPs []string `json:"blackListIPs"`
+	//AllowedConnnections are all IP addresses the server should expect a connection from. For example, the root server IP if you're running Grove from behind NGINX or Caddy
+	AllowedConnections []string `json:"allowedConnections"`
 
 }
 
